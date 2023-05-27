a297cb3926ba617e1c7f990088fc122f733fd91d
andreesgarcia2013 <18030514@itcelaya.edu.mx>
2023-05-26 23:52:29 -0600

    home screen implementado

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       theme: AppTheme(selectedColor: 5).getTheme(),
-      home: HomeScreen()
+      home: const HomeScreen()
     );
   }
 }
